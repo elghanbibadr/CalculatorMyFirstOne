b20d84022561a09c3f2bfbd8acb9475760dfc3c0
badr ghanbi <bghanbi50@gmail.com>
2022-04-04 21:35:34 +0000

    set up

---
+++ dist/index.c4775257.js
@@ -0,0 +1,7 @@
+const themeContainer = document.querySelector('.them-item');
+themeContainer.addEventListener('click', ()=>{
+    // themeContainer.classList.add('moveCircle');
+    console.log('clicked');
+});
+
+//# sourceMappingURL=index.c4775257.js.map
---
+++ main.js
@@ -0,0 +1,2 @@
+const display=document.querySelector('.display h1');
+console.log(display)
