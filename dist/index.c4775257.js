ffc5c7c7ec2b876d9c28a4d5a83d8915c3fadf9f
badr ghanbi <bghanbi50@gmail.com>
2022-04-04 23:08:26 +0000

    changeThem

--- dist/index.c4775257.js
+++ dist/index.c4775257.js
@@ -1,7 +1,2 @@
-const themeContainer = document.querySelector('.them-item');
-themeContainer.addEventListener('click', ()=>{
-    // themeContainer.classList.add('moveCircle');
-    console.log('clicked');
-});
 
 //# sourceMappingURL=index.c4775257.js.map
--- main.js
+++ main.js
@@ -1,2 +1,59 @@
-const display=document.querySelector('.display h1');
-console.log(display)
+const equalBtn=document.querySelector('.equal');
+const AC=document.querySelector('.reset');
+const Del=document.querySelector('.delete');
+let display=document.querySelector('.display h1');
+const btns=document.querySelectorAll('button[data-number]');
+const operation=document.querySelectorAll('button[data-operation]');
+let them=document.querySelector('.them-item');
+let circle=document.querySelector('.circle');
+let displayContainer=document.querySelector('.display');
+const body=document.body;
+let container=document.querySelector('.container');
+let calc=document.querySelector('h3');
+let theme=document.querySelector('h6');
+// them
+them.addEventListener('click',()=>{
+circle.classList.toggle('moveToEnd');
+body.classList.toggle('bodyChangeBg');
+ Del.classList.toggle('deleteBg');
+displayContainer.classList.toggle('containerBg');
+container.classList.toggle('containerBg');
+AC.classList.toggle('deleteBg');
+circle.classList.toggle('circleBg');
+equalBtn.classList.toggle('circleBg');
+calc.classList.toggle('textChange');
+theme.classList.toggle('textChange');
+})
+
+
+// update Display text content
+function updateDisplay(e){
+    display.innerText+=e.target.innerText;
+}
+
+
+// btns
+btns.forEach(btn=>btn.addEventListener('click',updateDisplay));
+operation.forEach(btn=>btn.addEventListener('click',(e)=>{
+    updateDisplay(e);
+}));
+
+// equal
+equalBtn.addEventListener('click',()=>{
+   
+   try{
+    display.innerText= Number(eval( display.innerText)).toFixed(2);
+   } 
+   catch{
+       display.innerText=`Ooops..!`;
+
+   }
+})
+
+//  delete actions
+ AC.addEventListener('click',()=>{
+     display.innerText='';
+ });
+  Del.addEventListener('click',()=>{
+   display.innerText=display.innerText.slice(0,-1);
+  })
